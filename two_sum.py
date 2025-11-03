1c9b3713e87f0c69aa2882d8c5a4bca4d1b533fe
Afonso Lopes <30830244+Tropax2@users.noreply.github.com>
2025-11-03 19:25:41 +0000

    Add files via upload

---
+++ two_sum.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def __init__(self):
+        self.result = []
+
+    def twoSum(self, nums, target):
+        self.nums = nums 
+        self.target = target
+        for i in range(len(self.nums)):
+            c = target - self.nums[i]
+            if c in nums and i!= nums.index(c):
+                self.result.append(i) 
+                self.result.append(nums.index(c))
+                break     
+        return self.result
+    
+s = Solution()
+print(s.twoSum([3, 2, 4], 6))
+
